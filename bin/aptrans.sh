2dd3934701b19c90f7a1ad830b2ea3f4e982efce
Filip Ilic <filip@filipilic.com>
2023-03-04 07:42:17 +0100

    translate

--- bin/aptrans.sh
+++ bin/aptrans.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cp /opt/centar/frappe/apps/bth/bin/trans13/sr.csv /opt/centar/frappe/apps/erpnext/erpnext/translations/sr.csv
-cp /opt/centar/frappe/apps/bth/bin/trans13/sr.csv  /opt/centar/frappe/apps/frappe/frappe/translations/sr.csv
-cp /opt/centar/frappe/apps/bth/bin/trans13/sr.csv  /opt/centar/frappe/apps/bth/bth/translations/sr.csv
-bench --site centar.belotehna.rs clear-cache
+cp /opt/centar/frappe/apps/gazda/bin/trans13/sr.csv /opt/centar/frappe/apps/erpnext/erpnext/translations/sr.csv
+cp /opt/centar/frappe/apps/gazda/bin/trans13/sr.csv  /opt/centar/frappe/apps/frappe/frappe/translations/sr.csv
+cp /opt/centar/frappe/apps/gazda/bin/trans13/sr.csv  /opt/centar/frappe/apps/gazda/gazda/translations/sr.csv
+bench --site stan.filipilic.com clear-cache
 
