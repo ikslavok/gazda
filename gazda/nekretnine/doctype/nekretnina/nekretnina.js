349898229f789d036e3078a165dee6ef815096d5
Filip Ilić <filip@filipilic.com>
2023-03-06 16:51:53 +0000

    workspace

--- gazda/nekretnine/doctype/nekretnina/nekretnina.js
+++ gazda/nekretnine/doctype/nekretnina/nekretnina.js
@@ -5,4 +5,8 @@ frappe.ui.form.on('Nekretnina', {
 	// refresh: function(frm) {
 
 	// }
+	
+	"embed_parcele": function(frm) {
+		frm.set_value("lokacija_parcele","<iframe name='embedMap' title='Adaptive EmbedMap' scrolling='no' frameborder='0' width='400px' height='400px' src='https://a3.geosrbija.rs/embedded/14ddaf8a-3b34-4a06-90a0-f775350c59cb' ></iframe>")
+	}
 });
--- gazda/nekretnine/doctype/transakcija/transakcija.js
+++ gazda/nekretnine/doctype/transakcija/transakcija.js
@@ -5,4 +5,37 @@ frappe.ui.form.on('Transakcija', {
 	// refresh: function(frm) {
 
 	// }
+	"state": function(frm) {
+			if (!frm.is_new()) {
+				frm.save();
+			}
+	},
+	"nekretnina": function(frm) {
+			frm.save();
+	},
+	"rok_za_plaćanje": function(frm) {
+			frm.save();
+	},
+	"datum_plaćanja": function(frm) {
+			if (!frm.is_new()) {
+				frm.save();
+			}
+	},
+	"tip_transakcije": function(frm) {
+			if (!frm.is_new()) {
+				frm.save();
+			}
+	},
+	"prihod": function(frm) {
+			frm.save();
+	},
+	"rashod": function(frm) {
+			frm.save();
+	},
+	"on_submit": function(frm) {
+		frm.set_value("state","PLAĆENO");
+	},
+	"validate": function(frm) {
+		frm.set_value('naziv',frm.doc.osoba + ' - ' + frm.doc.tip_transakcije + ' za ' + frm.doc.nekretnina);		
+	}
 });
