420df9e01ecd063dfc07afd78932da3ea624b0b3
Filip Ilić <filip@filipilic.com>
2025-03-02 21:01:28 +0000

    Add meter tracking feature to transaction types and properties

---
+++ gazda/nekretnine/doctype/nekretnina_brojila/nekretnina_brojila.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Filip Ilic and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class NekretninaBrojila(Document):
+	pass
